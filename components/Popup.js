d63bb53cc252de5c07864290dc84dde64a7d194a
Didac Bigorda <didacbigorda@gmail.com>
2019-04-02 16:55:52 +0200

    Add popup button and interaction

--- components/Popup.js
+++ components/Popup.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import { Text, Button } from 'react-native';
 
 export default class Popup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pressed: false,
+    }
+  }
+
   render() {
     return (
       <>
         <Text>
           Hola
         </Text>
-        <Button title="Push me!" /> 
+        <Button 
+          title="Push me!"
+          onPress={() => this.setState({ pressed: !this.state.pressed })} 
+        />
+        {this.state.pressed ? <Text>Pushed</Text> : <Text>Not pushed</Text>}
       </>
     )
   }
